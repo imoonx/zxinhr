9003817dd3814900f4e0e9640a1ab4206300579a
xuan <362386736@qq.com>
2018-09-06 15:01:39 +0800

    测试样式

---
+++ js/newsList.js
@@ -0,0 +1,85 @@
+var newsList = [{
+    id: 1,
+    name: "专职",
+    item: [{
+        id: 1,
+        name: "可折叠手机要来了？三星：产品惊艳只有我们能做",
+        img: "../imgs/imgs/123.png",
+        url: ""
+    }, {
+        id: 2,
+        name: "华为P20系列新配色今日开售：3388元起",
+        img: "../imgs/imgs/456.png",
+        url: ""
+    }]
+}, {
+    id: 2,
+    name: "兼职",
+    item: [{
+        id: 1,
+        name: "可折叠手机要来了？三星：产品惊艳只有我们能做",
+        img: "../imgs/imgs/123.png",
+        url: ""
+    }, {
+        id: 2,
+        name: "华为P20系列新配色今日开售：3388元起",
+        img: "../imgs/imgs/456.png",
+        url: ""
+    }],
+}, {
+    id: 3,
+    name: "合作院校",
+    item: [{
+        id: 1,
+        name: "可折叠手机要来了？三星：产品惊艳只有我们能做",
+        img: "../imgs/imgs/123.png",
+        url: ""
+    }, {
+        id: 2,
+        name: "华为P20系列新配色今日开售：3388元起",
+        img: "../imgs/imgs/456.png",
+        url: ""
+    }],
+}, {
+    id: 4,
+    name: "合作单位",
+    item: [{
+        id: 1,
+        name: "可折叠手机要来了？三星：产品惊艳只有我们能做",
+        img: "../imgs/imgs/123.png",
+        url: ""
+    }, {
+        id: 2,
+        name: "华为P20系列新配色今日开售：3388元起",
+        img: "../imgs/imgs/456.png",
+        url: ""
+    }],
+}, {
+    id: 5,
+    name: "成功案例",
+    item: [{
+        id: 1,
+        name: "可折叠手机要来了？三星：产品惊艳只有我们能做",
+        img: "../imgs/imgs/123.png",
+        url: ""
+    }, {
+        id: 2,
+        name: "华为P20系列新配色今日开售：3388元起",
+        img: "../imgs/imgs/456.png",
+        url: ""
+    }],
+}, {
+    id: 6,
+    name: "常见问题",
+    item: [{
+        id: 1,
+        name: "可折叠手机要来了？三星：产品惊艳只有我们能做",
+        img: "../imgs/imgs/123.png",
+        url: ""
+    }, {
+        id: 2,
+        name: "华为P20系列新配色今日开售：3388元起",
+        img: "../imgs/imgs/456.png",
+        url: ""
+    }],
+}];
